0e66468e7f2926a33f6a82e076896bcdf0068645
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-02 19:22:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ function iLoveTheBeatles(num){
   return strings;
 }
 
-const facts = ["He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"]
-
 function johnLennonFacts(facta){
   var newArray =[];
   var i = 0;
